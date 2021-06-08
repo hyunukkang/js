cc602d8da1728f732c059aca6ef6053b6c843113
hyunuk kang <hyunuk.green@gmail.com>
2021-06-09 01:54:59 +0900

    complete

--- 0607/basic-layout/script.js
+++ 0607/basic-layout/script.js
@@ -8,7 +8,7 @@
 // }
 
 var toggle = document.getElementById('toggle');
-var navbar = document.querySelector('.navbar')
+var navbar = document.querySelector('.navbar');
 console.log(toggle);
 console.log(navbar);
 
@@ -17,7 +17,7 @@ toggle.addEventListener('click', function(){
     this.classList.toggle('active');
     navbar.classList.toggle('active');
     // navbar에 classList에 active 붙여줘
-})
+});
 
 /* arrow top 구현 */
 /* 
@@ -27,15 +27,40 @@ toggle.addEventListener('click', function(){
 4. arrow top 을 클릭하면 스크롤을 최상단으로 올린다
     addEventLIstener('scroll', function(){})
 */
+// >>>>>>>>>>>>0607
+// const arrowTop = document.getElementById('arrowTop');
+// document.addEventListener('scroll', function(){
+//     console.log('>>>>> scroll');
+//     console.log(window.scrollY);
+//     if(window.scrollY > 73){
+//         arrowTop.classList.add('visible');
+//     }else{
+//         arrowTop.classList.remove('visible')
+//     }
+// })
+// />>>>>>>>>>>>>>>>> 0608
+// header fix
 const arrowTop = document.getElementById('arrowTop');
+const aboutSection = document.getElementById('about');
+const aboutHeight = aboutSection.clientHeight;
+// clientHeight = 스크롤바 section 높이 구하기 - 검색 알아봄
+const header = document.getElementById('header');
+
 document.addEventListener('scroll', function(){
     console.log('>>>>> scroll');
     console.log(window.scrollY);
-    if(window.scrollY > 73){
+    if(window.scrollY > aboutHeight){
         arrowTop.classList.add('visible');
     }else{
         arrowTop.classList.remove('visible')
     }
+    // 스크롤이 되었을때 header 가 fixed가 되어라
+    if(window.scrollY > 0){
+        header.classList.add('fixedHead');
+    }else{
+        header.classList.remove('fixedHead');
+    }
+    // 랜더링 한번되고 실행되서 오류 났었음
 })
 
 // arrow top 클릭 했을떄 scroll이 제일 위로 올라가게
@@ -47,6 +72,46 @@ arrowTop.addEventListener('click', function(event){
     // 그러므로 preventDefault()로 막는다
     console.log(event);
     event.preventDefault();
-    console.log('>>>>> arrow top')
-    window.scrollTo({top: 0, behavior: 'smooth'})
+    console.log('>>>>> arrow top');
+    window.scrollTo({top: 0, behavior: 'smooth'});
 })
+
+// navbar 클릭시 스크롤 이동
+var navbarItem = document.querySelectorAll('.navbar__item a');
+// var aboutSection = document.querySelector('#about');
+console.log(navbarItem);
+
+navbarItem.forEach(function(item){
+   
+    item.addEventListener('click', function(event){
+        event.preventDefault();
+        console.log(event);
+
+        toggle.classList.remove('active');
+        navbar.classList.remove('active');
+
+        // about
+        // skills
+        // portfolio
+
+        // "" 해당화면으로이동
+        // 위 event콘솔에서 확인 : target 안에 dataset이 존재
+        var target = event.target.dataset.link;
+
+        console.log(target);
+        var element = document.querySelector(target);
+        console.log(element);
+        element.scrollIntoView({behavior: "smooth"});
+        // aboutSection.scrollIntoView({behavior: "smooth"});
+    })  
+
+
+    
+})
+
+// navbarItem[0].addEventListener('click', function(event){
+//     event.preventDefault();
+//     navbarItem[0].scrollIntoView({behavior: "smooth"});
+// }
+
+// element.scrollIntoView()
